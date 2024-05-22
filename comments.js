693cbde04c2c9060ee18ccc79b22c2c366598346
Yves Manalo <23yvesmanalo@gmail.com>
2024-05-22 11:36:08 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,66 +1,41 @@
 // Create web server
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
-var querystring = require('querystring');
-var path = require('path');
+// 1. Create a web server
+// 2. Create a route for /comments
+// 3. Set response header to 'application/json'
+// 4. Create an array of objects with comments (id, username, comment)
+// 5. Send JSON response with the array of comments
+// 6. Listen on port 3000
 
-// Function to handle requests
-function requestHandler(req, res) {
-  // Parse the request url
-  var parsedUrl = url.parse(req.url);
-  // Get the path name
-  var pathName = parsedUrl.pathname;
-  // Get the query string as an object
-  var query = querystring.parse(parsedUrl.query);
-  // Get the method
-  var method = req.method;
+const http = require('http');
 
-  // If the request is a POST request
-  if (method === 'POST' && pathName === '/comments') {
-    // Create a variable to store the data
-    var body = '';
-    // Listen for data
-    req.on('data', function(data) {
-      body += data;
+const server = http.createServer((req, res) => {
+  if (req.url === '/comments') {
+    res.writeHead(200, {
+      'Content-Type': 'application/json',
     });
-    // When the request ends
-    req.on('end', function() {
-      // Parse the body
-      var comment = querystring.parse(body);
-      // Read the comments from the file
-      fs.readFile('comments.json', function(err, data) {
-        if (err) {
-          // If the file doesn't exist
-          if (err.code === 'ENOENT') {
-            // Create an empty array
-            var comments = [];
-            // Add the comment to the array
-            comments.push(comment);
-            // Write the comments to the file
-            fs.writeFile('comments.json', JSON.stringify(comments), function(err) {
-              if (err) {
-                // Send an error response
-                res.writeHead(500, { 'Content-Type': 'text/plain' });
-                res.end('Internal Server Error');
-              } else {
-                // Send a success response
-                res.writeHead(201, { 'Content-Type': 'text/plain' });
-                res.end('Created');
-              }
-            });
-          } else {
-            // Send an error response
-            res.writeHead(500, { 'Content-Type': 'text/plain' });
-            res.end('Internal Server Error');
-          }
-        } else {
-          // Parse the comments
-          var comments = JSON.parse(data);
-          // Add the comment to the array
-          comments.push(comment);
-          // Write the comments to the file
-          fs.writeFile('comments.json', JSON.stringify(comments), function(err) {
-            if (err) {
-              // Send an error response
-              res.writeHead(500,
+
+    const comments = [
+      {
+        id: 1,
+        username: 'john_doe',
+        comment: 'Hello, World!',
+      },
+      {
+        id: 2,
+        username: 'jane_doe',
+        comment: 'Hello, Node.js!',
+      },
+    ];
+
+    res.end(JSON.stringify(comments));
+  } else {
+    res.writeHead(404, {
+      'Content-Type': 'text/plain',
+    });
+    res.end('Not found');
+  }
+});
+
+server.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
